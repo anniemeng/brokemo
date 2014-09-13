475c97468f8399c6f91ca3d915b3b8c1c4bf1d11
anniemeng <anniezmeng@gmail.com>
2014-09-13 00:13:01 -0400

    added js

---
+++ public/js/charges.js
@@ -0,0 +1,10 @@
+var Charge = Parse.Object.extend("Charge", {
+    defaults: {
+      payer: "",
+      reciever: "",
+      isDone: false,
+      image: null,
+      type: "",
+      descrip: ""
+    },
+})
---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+  Parse.initialize("EbdHBkoWKSqOcEMPf6QWb4R8tP6xxrvRw0h6xG2v", "BnsnS6EjkzATZRrmQKSEXsHQYw3xWg9tnbsUARbi");
+
+  //models
+  var Charge = Parse.Object.extend("Charge", {
+      defaults: {
+        payer: "",
+        reciever: "",
+        isDone: false,
+        image: null,
+        type: "",
+        descrip: ""
+      },
+  })
+});
